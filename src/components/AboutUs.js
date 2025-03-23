0739bb7726f31e0fa7dc6c701cc381c1212b2185
geoffn7 <geoffrey.nguyen9@gmail.com>
2025-03-23 10:38:42 -0400

    Add slide in effect to About Us page and update font to look nicer

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,11 +1,14 @@
 // src/components/AboutUs.js
 import React from 'react';
+import './AboutUs.css';  // Import the updated CSS file
 
 const AboutUs = () => {
   return (
-    <div>
-      <h2>About Us</h2>
-      <p>Welcome to our about page. Here's some information about our company.</p>
+    <div className="about-us-container">
+      <h2 className="about-title">Our Story</h2>
+      <p className="about-description">
+        Welcome to our about page. Here's some information about our company. We are dedicated to providing the best products and services to our customers, and we are passionate about sustainability and quality.
+      </p>
     </div>
   );
 };
